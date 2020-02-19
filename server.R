826930c5aa1c59d2d4ed623c16d60f0c1604f26e
Christian Koenig <christian.koenig@hu-berlin.de>
2020-02-19 13:30:24 +0100

    Full import functionality. Fixed bug where imported model settings are not displayed in the settings table under "3. Model".

--- server.R
+++ server.R
@@ -8,14 +8,14 @@ library(tidyverse)
 library(rangeModelMetadata)
 
 odmap_dict = read.csv("www/odmap_dict.csv", header = T, stringsAsFactors = F)
-rmm_dict = read.csv("www/rmm_dict.csv", header = T, stringsAsFactors = F) # TODO Change to pull table directly from package
+rmm_dict = rmmDataDictionary()
 
 server <- function(input, output, session) {
   # ------------------------------------------------------------------------------------------#
   #                           Rendering functions for UI elements                             # 
   # ------------------------------------------------------------------------------------------#
   render_text = function(element_id, element_placeholder){
-    textAreaInput(inputId = element_id, placeholder = element_placeholder, label = NULL, resize = "vertical")
+    textAreaInput(inputId = element_id, placeholder = element_placeholder, label = NULL, height = "45px", resize = "vertical")
   }
   
   render_authors = function(){
@@ -37,14 +37,25 @@ server <- function(input, output, session) {
   }
   
   render_extent = function(element_id){
-    splitLayout(
-      cellWidths = c("110px",  "110px", "110px", "110px", "110px"),
-      p("Spatial extent", style = "padding: 9px 12px"),
-      textAreaInput(inputId = paste0(element_id, "_xmin"), placeholder = "xmin", label = NULL, resize = "none"),
-      textAreaInput(inputId = paste0(element_id, "_xmax"), placeholder = "xmax", label = NULL, resize = "none"),
-      textAreaInput(inputId = paste0(element_id, "_ymin"), placeholder = "ymin", label = NULL, resize = "none"),
-      textAreaInput(inputId = paste0(element_id, "_ymax"), placeholder = "ymax", label = NULL, resize = "none")
-    )
+    if(element_id == "o_scale_1"){
+          splitLayout(
+            cellWidths = c("170px",  "150px", "150px", "150px", "150px"),
+            p("Spatial extent (long/lat)", style = "padding: 9px 12px"),
+            textAreaInput(inputId = paste0(element_id, "_xmin"), placeholder = "xmin", label = NULL, height = "45px", resize = "none"),
+            textAreaInput(inputId = paste0(element_id, "_xmax"), placeholder = "xmax", label = NULL, height = "45px", resize = "none"),
+            textAreaInput(inputId = paste0(element_id, "_ymin"), placeholder = "ymin", label = NULL, height = "45px", resize = "none"),
+            textAreaInput(inputId = paste0(element_id, "_ymax"), placeholder = "ymax", label = NULL, height = "45px", resize = "none")
+      )
+    } else {
+          splitLayout(
+            cellWidths = c("120px",  "150px", "150px", "150px", "150px"),
+            p("Spatial extent", style = "padding: 9px 12px"),
+            textAreaInput(inputId = paste0(element_id, "_xmin"), placeholder = "xmin", label = NULL, height = "45px", resize = "none"),
+            textAreaInput(inputId = paste0(element_id, "_xmax"), placeholder = "xmax", label = NULL, height = "45px", resize = "none"),
+            textAreaInput(inputId = paste0(element_id, "_ymin"), placeholder = "ymin", label = NULL, height = "45px", resize = "none"),
+            textAreaInput(inputId = paste0(element_id, "_ymax"), placeholder = "ymax", label = NULL, height = "45px", resize = "none")
+      )
+    }
   }
   
   render_model_algorithm = function(element_id, element_placeholder){
@@ -53,6 +64,7 @@ server <- function(input, output, session) {
   
   render_model_settings = function(){
     div(
+      em(p("Edit fields by double clicking in the table. Add new settings with the plus sign.", style = "font-weight: 300;")),
       tabsetPanel(id = "settings_tabset"),
       actionButton("add_setting", label = NULL, icon = icon("plus")),
       br(),br()
@@ -199,7 +211,7 @@ server <- function(input, output, session) {
   }
   
   import_odmap_to_authors = function(element_id, values){
-    if(nrow(authors_df) == 0 | input[["replace_values"]] == "Yes"){
+    if(nrow(authors$df) == 0 | input[["replace_values"]] == "Yes"){
       names_split = unlist(strsplit(values, split = "; "))
       names_split = regmatches(names_split, regexpr(" ", names_split), invert = TRUE)
       authors$df = authors$df[0,] # Delete previous entries
@@ -223,38 +235,18 @@ server <- function(input, output, session) {
     values = gsub("(^.*)( \\(xmin, xmax, ymin, ymax\\)$)", "\\1", values)
     values_split = unlist(strsplit(values, ", "))
     names(values_split) = c("xmin", "xmax", "ymin", "ymax")
-    for(i in 1:length(values_plit)){
+    for(i in 1:length(values_split)){
       if(input[[paste0(element_id, "_", names(values_split[i]))]] == "" | input[["replace_values"]] == "Yes"){
-        updateTextAreaInput(session = session, inputId = paste0(element_id, "_", names(values_split[i])), value = values_split[i])    
+        updateTextAreaInput(session = session, inputId = paste0(element_id, "_", names(values_split[i])), value = as.numeric(values_split[i]))
       }
     }
   }
-  
   
   import_odmap_to_model_algorithm = function(element_id, values){
     if(length(input[[element_id]]) == 0 | input[["replace_values"]] == "Yes"){
       values = unlist(strsplit(values, split = "; "))
       suggestions_new =  sort(trimws(c(model_settings$suggestions, as.character(values))))
       updateSelectizeInput(session = session, inputId = element_id, choices = suggestions_new, selected = values)  
-    }
-  }
-  
-  import_odmap_to_model_setting = function(element_id, values){
-    settings_all = unlist(strsplit(values, split = "; "))
-    for(settings_tmp in settings_all){
-      if(grepl("no settings provided", settings_tmp)){next}
-      settings_split = unlist(regmatches(settings_tmp, regexpr(": ", settings_tmp), invert = TRUE)) # split at first instance of ":"
-      alg = settings_split[1]
-      settings = unlist(strsplit(settings_split[2], split = "\\), ")) # "), " is a more reliable split string than ","
-      settings[length(settings)] = substr(settings[length(settings)], 1, nchar(settings[length(settings)])-1) # remove trailing parenthesis
-      settings_df = data.frame(setting = gsub("(^.*)( \\()(.*)", "\\1", settings), value = gsub("(^.*)( \\()(.*)", "\\3", settings), stringsAsFactors = F)
-      if(is.null(model_settings[[alg]]) | input[["replace_values"]] == "Yes"){
-        model_settings[[alg]] = settings_df
-      } else {
-        settings_old = model_settings[[alg]] %>% filter(!is.na(value))
-        settings_df = settings_df %>% filter(!setting %in% settings_old$setting)
-        model_settings[[alg]] = full_join(settings_old, settings_df, by = c("setting", "value"))
-      }
     }
   }
   
@@ -289,8 +281,22 @@ server <- function(input, output, session) {
     }
   }
   
-  import_rmm_to_algorithm = function(imported_values){
-    
+  # Generic import functions
+  import_model_settings = function(element_id, values){
+    settings_all = unlist(strsplit(values, split = "; "))
+    algorithms = c()
+    for(settings_tmp in settings_all){
+      if(grepl("no settings provided", settings_tmp)){next}
+      settings_split = unlist(regmatches(settings_tmp, regexpr(": ", settings_tmp), invert = TRUE)) # split at first instance of ":"
+      alg = settings_split[1]
+      algorithms = c(algorithms, alg)
+      settings = unlist(strsplit(settings_split[2], split = "\\), ")) # "), " is a more reliable split string than ","
+      settings[length(settings)] = substr(settings[length(settings)], 1, nchar(settings[length(settings)])-1) # remove trailing parenthesis
+      settings_df = data.frame(setting = gsub("(^.*)( \\()(.*)", "\\1", settings), value = gsub("(^.*)( \\()(.*)", "\\3", settings), stringsAsFactors = F)
+      model_settings_import[["algorithms"]] = c(model_settings_import[["algorithms"]], alg)
+      model_settings_import[[alg]] = settings_df
+    }
+    updateSelectizeInput(session = session, inputId = "o_algorithms_1", selected = algorithms)
   }
   
   # ------------------------------------------------------------------------------------------#
@@ -365,7 +371,9 @@ server <- function(input, output, session) {
       if(length(all_elements) == 0){
         next 
       } else {
-        completed_elements = sum(sapply(all_elements, function(x){!(identical(input[[x]], "") | identical(input[[x]], NULL))}, USE.NAMES = T, simplify = T))
+        completed_elements = sum(sapply(all_elements, function(x){
+          !(identical(input[[x]], "") | identical(input[[x]], 0) | identical(input[[x]], NULL))
+        }, USE.NAMES = T, simplify = T))
         progress[sect] = (sum(completed_elements) / length(all_elements)) * 100
       }
     }
@@ -382,6 +390,7 @@ server <- function(input, output, session) {
   
   output$protocol_download = downloadHandler(
     filename = function(){
+      # TODO improve file naming
       paste0("ODMAP_protocol_", Sys.Date(), ".", input$document_format)
     },
     content = function(file){
@@ -528,31 +537,34 @@ server <- function(input, output, session) {
   # Model algorithms and settings
   model_settings = reactiveValues(suggestions = rmm_dict %>% filter(field1 == "model" & field2 == "algorithm") %>% pull(field3) %>% unique() %>% trimws(),
                                   settings_tabset = NULL)
+  model_settings_import = reactiveValues(algorithms = character(0))
   
   observeEvent(input$o_algorithms_1, {
     if(length(input$o_algorithms_1) > length(model_settings$settings_tabset)) { # New algorithm selected
       new_algs = setdiff(input$o_algorithms_1, model_settings$settings_tabset)
       for(new_alg in new_algs){
-          # Create dataframe for new algorithm
-          if(new_alg %in% filter(rmm_dict, field2 == "algorithm")$field3){
-            model_settings[[new_alg]] = rmm_dict %>%
-              filter(field1 == "model" & field2 == "algorithm" & field3 == new_alg) %>%
-              mutate(setting = entity, value = as.character(NA)) %>%
-              dplyr::select(setting, value)
-          } else {
-            model_settings[[new_alg]] = data.frame(setting = character(0), value = character(0))
-          }
-          
-          # Add new dataframe to output and settings_tabset
-          local({ # Needs local evaluation because of asynchronous execution of renderDT
-            .new_alg = new_alg
-            output[[.new_alg]] = renderDT(model_settings[[.new_alg]], editable = T, rownames = F,
-                                          options = list(dom = "t", pageLength = 50, autoWidth = T, columnDefs = list(list(width = '50%', targets = "_all"))))
-            observeEvent(input[[paste0(.new_alg, '_cell_edit')]], {
-              model_settings[[.new_alg]][input[[paste0(.new_alg, '_cell_edit')]]$row, input[[paste0(.new_alg, '_cell_edit')]]$col + 1] = input[[paste0(.new_alg, '_cell_edit')]]$value
-            })
+        # Create dataframe for new algorithm
+        if(new_alg %in% model_settings_import[["algorithms"]]){
+          model_settings[[new_alg]] = model_settings_import[[new_alg]]
+        } else if(new_alg %in% filter(rmm_dict, field2 == "algorithm")$field3){
+          model_settings[[new_alg]] = rmm_dict %>%
+            filter(field1 == "model" & field2 == "algorithm" & field3 == new_alg) %>%
+            mutate(setting = entity, value = as.character(NA)) %>%
+            dplyr::select(setting, value)
+        } else {
+          model_settings[[new_alg]] = data.frame(setting = character(0), value = character(0))
+        }
+        
+        # Add new dataframe to output and settings_tabset
+        local({ # Needs local evaluation because of asynchronous execution of renderDT
+          .new_alg = new_alg
+          output[[.new_alg]] = renderDT(model_settings[[.new_alg]], editable = T, rownames = F,
+                                        options = list(dom = "t", pageLength = 50, autoWidth = T, columnDefs = list(list(width = '50%', targets = "_all"))))
+          observeEvent(input[[paste0(.new_alg, '_cell_edit')]], {
+            model_settings[[.new_alg]][input[[paste0(.new_alg, '_cell_edit')]]$row, input[[paste0(.new_alg, '_cell_edit')]]$col + 1] = input[[paste0(.new_alg, '_cell_edit')]]$value
           })
-          appendTab(inputId = "settings_tabset", select = T, tab = tabPanel(title = new_alg, value = new_alg, DTOutput(outputId = new_alg)))
+        })
+        appendTab(inputId = "settings_tabset", select = T, tab = tabPanel(title = new_alg, value = new_alg, DTOutput(outputId = new_alg)))
       }
       model_settings$settings_tabset = input$o_algorithms_1 # update name list of displayed tabs
     } else {
@@ -651,12 +663,12 @@ server <- function(input, output, session) {
              suggestion = import_odmap_to_suggestion(element_id = protocol_upload$element_id[i], values = protocol_upload$Value[i]),
              extent = import_odmap_to_extent(element_id = protocol_upload$element_id[i], values = protocol_upload$Value[i]),
              model_algorithm = import_odmap_to_model_algorithm(element_id = protocol_upload$element_id[i], values = protocol_upload$Value[i]),
-             model_setting = import_odmap_to_model_setting(element_id = protocol_upload$element_id[i], values = protocol_upload$Value[i]))
+             model_setting = import_model_settings(element_id = protocol_upload$element_id[i], values = protocol_upload$Value[i]))
     }
     
     # Switch to "Create a protocol" 
     reset("upload")
-    updateNavbarPage(session, "navbar", selected = "tab_2")
+    updateNavbarPage(session, "navbar", selected = "create")
     updateTabsetPanel(session, "Tabset", selected = "Overview")
   })
   
@@ -679,6 +691,18 @@ server <- function(input, output, session) {
       }
     }
     
+    model_settings = protocol_upload %>% 
+      dplyr::filter(Field.1 == "model" & Field.2 == "algorithm") %>% 
+      group_by(Field.3) %>% 
+      summarize(settings_string = paste(paste0(Entity, " (", Value, ")"), collapse = ", "))
+    
+    if(nrow(model_settings) > 0){
+      imported_values[["m_settings_1"]] = model_settings %>% 
+        ungroup() %>% 
+        summarize(x = paste(paste0(Field.3, ": ", settings_string), collapse = "; ")) %>% 
+        pull(x)
+    }
+    
     # 2. Update ODMAP input fields with imported values
     for(i in 1:length(imported_values)){
       switch(odmap_dict$element_type[which(odmap_dict$element_id == names(imported_values)[i])],
@@ -686,11 +710,12 @@ server <- function(input, output, session) {
              author = import_rmm_to_authors(element_id = names(imported_values)[i], values = imported_values[[i]]),
              suggestion = import_rmm_to_suggestion(element_id = names(imported_values)[i], values = imported_values[[i]]),
              extent = import_rmm_to_extent(element_id = names(imported_values)[i], values = imported_values[[i]]),
-             model_algorithm = import_rmm_to_algorithm(imported_values)[i], protocol = protocol_upload)
+             model_setting = import_model_settings(element_id = names(imported_values)[i], values = imported_values[[i]]))
     }
     
     # Switch to "Create a protocol" 
     reset("upload")
-    updateNavbarPage(session, "navbar", selected = "tab_2")
+    updateNavbarPage(session, "navbar", selected = "create")
+    updateTabsetPanel(session, "Tabset", selected = "Overview")
   })
 }
