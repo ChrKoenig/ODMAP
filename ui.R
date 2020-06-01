d5dee4578d63704a7db7105b4627c95cf38acb4d
damariszurell <damaris@zurell.de>
2020-06-01 20:11:08 +0200

    Update citation and link to paper

--- ui.R
+++ ui.R
@@ -36,17 +36,18 @@ ui <-  tagList(
                p("However, models must be fit for purpose, with all important aspects of their development and applications properly considered. 
              Despite the widespread use of SDMs, the standardisation and documentation of model protocols remains limited. To address these issues, 
              we propose a standard protocol for reporting SDMs. We call this the ODMAP (Overview, Data, Model, Assessment and Prediction) protocol
-             as each of its components reflects the main steps involved in building SDMs and other empirically-based biodiversity models.", style= "font-size: 18px;"), 
+             as each of its components reflects the main steps involved in building SDMs and other empirically-based biodiversity models (",a(href = 'https://doi.org/10.1111/ecog.04960', 'Zurell et al. 2020', target = "_blank", .noWS = "outside"),").", style= "font-size: 18px;"), 
                img(src = "workflow.jpg", width = "60%", style="display: block; margin-left: auto; margin-right: auto; min-width: 500px;"), br(),
                p("The ODMAP protocol serves two main purposes. First, it provides a checklist for authors detailing key steps for model building and analyses. 
              Second, it introduces a standard approach to documentation that ensures transparency and reproducibility, facilitating peer review and 
              expert evaluation of model quality as well as meta-analyses.", style= "font-size: 18px;"),
                p("This Shiny web application helps to implement the ODMAP approach and produces well formatted protocols that can be exported for further usage. 
-                 For further explanation please refer to the original publication (Zurell et al., accepted).", style= "font-size: 18px;"),
+                 For further explanation please refer to the original publication (Zurell et al. 2020).", style= "font-size: 18px;"),
                em(p("Please cite as follows:", style = "font-size: 18px;")),
                p("Zurell D, Franklin J, König C, Bouchet PJ, Serra-Diaz JM, Dormann CF, Elith J, Fandos Guzman G, Feng X, Guillera-Arroita G, Guisan A, Leitão PJ, 
-                 Lahoz-Monfort JJ, Park DS, Peterson AT,  Rapacciuolo G, Schmatz DR, Schröder B, Thuiller W, Yates KL, Zimmermann NE, Merow C (accepted) 
-                 A standard protocol for describing species distribution models. Ecography. DOI: 10.1111/ecog.04960.", style= "font-size: 18px;")
+                 Lahoz-Monfort JJ, Park DS, Peterson AT,  Rapacciuolo G, Schmatz DR, Schröder B, Thuiller W, Yates KL, Zimmermann NE, Merow C (2020) 
+                 A standard protocol for describing species distribution models. Ecography. DOI: ",
+                 a(href = 'https://doi.org/10.1111/ecog.04960', '10.1111/ecog.04960', target = "_blank", .noWS = "outside"), style= "font-size: 18px;")
         )),
       column(width = 2)
     )),
